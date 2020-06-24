05d97a8cf56dc9c20b5225abd5e7d8cbc2536da1
ya-alexander <alexander.huber@software-architects.at>
2020-06-24 08:08:31 +0200

    Update connect.ps1

--- provisioning/connect.ps1
+++ provisioning/connect.ps1
@@ -1,5 +1,5 @@
-$tenantId = "70f564a8-40f6-4ecb-a4e6-50ab9e98d0e3"
-$subscriptionId = "8cc2596b-4839-40cb-9cd1-d40181e56147"
+$tenantId = "00000000-0000-0000-0000-000000000000"
+$subscriptionId = "00000000-0000-0000-0000-000000000000"
 
 Connect-AzAccount `
     -TenantId $tenantId `
